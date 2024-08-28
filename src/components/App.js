32d0d38e5604df75362df7bfaa743aa7639cb4f8
Allovallo <grossco@meta.ua>
2024-08-28 16:38:29 +0300

    Page About updated

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,20 @@ import { Route, Routes } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { SharedLayout } from './SharedLayout';
 import { About } from '../pages/About';
+import { Mission } from './Mission';
+import { Team } from './Team';
+import { Reviews } from './Reviews';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
+        <Route path="/about" element={<About />}>
+          <Route path="mission" element={<Mission />}></Route>
+          <Route path="team" element={<Team />}></Route>
+          <Route path="reviews" element={<Reviews />}></Route>
+        </Route>
       </Route>
     </Routes>
   );
