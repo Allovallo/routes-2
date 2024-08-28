b1f9a2d7884d1383b88f9427c08dda303721ffcb
Allovallo <grossco@meta.ua>
2024-08-28 16:24:40 +0300

    files updated

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import { Home } from '../pages/Home';
+import { SharedLayout } from './SharedLayout';
+import { About } from '../pages/About';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
