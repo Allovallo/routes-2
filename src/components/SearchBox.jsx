9e1ab937d4f6458f8193c03c47efb4978d1b71b7
Allovallo <grossco@meta.ua>
2024-08-30 14:37:09 +0300

    files updated

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,7 +1,14 @@
-export const SearchBox = ({value, onChange}) => {
+import { Wrapper, Input, Icon } from "./SearchBox.styled"
+
+export const SearchBox = ({ value, onChange }) => {
     return (
-        <div>
-            <input type="text" value={value} onChange={e => onChange(e.target.value)}></input>
-        </div>
+        <Wrapper>
+            <Icon/>
+            <Input
+                type="text"
+                value={value}
+                onChange={e => onChange(e.target.value)}
+            />
+        </Wrapper>
     )
 }
