367d216f70f82a7aa2aeb5793f071f87b5d4ecae
Allovallo <grossco@meta.ua>
2024-08-30 14:11:55 +0300

    Search added

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,10 +1,12 @@
 import { ProductList } from "../components/ProductList";
 import { getProducts } from "../fakeAPI";
+import { SearchBox } from "../components/SearchBox";
 
 export const Products = () => {
     const products = getProducts();
     return (
         <main>
+            <SearchBox/>
             <ProductList products={products} />
         </main>
     )
