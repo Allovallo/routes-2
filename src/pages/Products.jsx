3d9d70e27e5952c28f2f7b457d55bc12034ccd5d
Allovallo <grossco@meta.ua>
2024-08-31 17:03:37 +0300

    BackLink added

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -6,21 +6,23 @@ import { SearchBox } from "../components/SearchBox";
 export const Products = () => {
     const products = getProducts();
     const [searchParams, setSearchParams] = useSearchParams();
-    const productName = searchParams.get('name') ?? '';
+    const productName = searchParams.get('name') ?? "";
 
-    const visibleProducts = products.filter((product) =>
+    const visibleProducts = products.filter((product) => 
         product.name.toLowerCase().includes(productName.toLowerCase())
-    );
+    )
 
     const updateQueryString = name => {
         const nextParams = name !== "" ? { name } : {};
-        setSearchParams(nextParams)
+        setSearchParams(nextParams);
     }
 
     return (
         <main>
-            <SearchBox value={productName} onChange={updateQueryString}/>
-            <ProductList products={visibleProducts} />
+            <SearchBox value={ productName} onChange={updateQueryString}/>
+            <ProductList products={ visibleProducts }/>
         </main>
     )
 }
+
+
